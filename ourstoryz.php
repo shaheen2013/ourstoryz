e3e825963d92b6668bdff33f2a86cb81a9b1e75b
imran-hossain <engg.imran22@gmail.com>
2024-06-06 15:49:28 +0600

    debuging

--- ourstoryz.php
+++ ourstoryz.php
@@ -1129,8 +1129,7 @@ function keepsakealbum_data_by_guest($atts)
 
     // Fetch related events data
     $album_data = fetch_keepsakealbum_data_by_display_type($event_id, $storyz_id, $display_type);
-    var_dump($album_data);
-    die();
+     
     // Check if album data is available
     
     $all = $album_data['data']['keepsakeAlbum'];
