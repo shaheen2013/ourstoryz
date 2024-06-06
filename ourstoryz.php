b8b915bfa4cd5eb849d53d7ddc4aafc446b31732
imran-hossain <engg.imran22@gmail.com>
2024-06-06 17:37:17 +0600

    guest name and profile show

--- ourstoryz.php
+++ ourstoryz.php
@@ -1128,7 +1128,7 @@ function keepsakealbum_data_by_guest($atts)
     // Check if album data is available
     if (!empty($album_data['data']['keepsakeAlbum'])) {
       $all = $album_data['data']['keepsakeAlbum'];
-      var_dump($all);
+      var_dump($all['guest_profile']);
       die();
       // Start HTML output
       $output = '';
