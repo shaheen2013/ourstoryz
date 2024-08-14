9d92de74cf1d4bb20a616a8b0ceda628733df61d
imran-bin <engg.imran22@gmail.com>
2024-08-14 13:17:27 +0600

    working on google recaptcha v3

--- includes/class-signup-modal.php
+++ includes/class-signup-modal.php
@@ -129,7 +129,43 @@ function ourstoryz_shortcode_function()
                         </div>
                     </div>
 
-              
+                    <script>
+                        function handleCaptchaVerification() {
+                            grecaptcha.ready(function() {
+                                grecaptcha.execute('6LdoHyMqAAAAADoxXp6VJMHKXQCHlg5x90f0W5Ph', {
+                                    action: 'submit'
+                                }).then(function(token) {
+                                    // Call the function to verify the token using jQuery AJAX
+                                    verifyCaptchaTokenWithjQuery(token);
+                                });
+                            });
+                        }
+
+                        function verifyCaptchaTokenWithjQuery(token) {
+                            // Use jQuery AJAX to send the token to the server for verification
+                            jQuery.ajax({
+                                url: '<?php echo admin_url("admin-ajax.php"); ?>',
+                                type: 'POST',
+                                data: {
+                                    action: 'verify_recaptcha',
+                                    recaptcha_token: token
+                                },
+                                success: function(response) {
+                                    var data = JSON.parse(response);
+                                    if (data.success && data.score >= 0.5) {
+                                        // User is human, proceed to the next section
+                                        handleSetModal('want-to-test-section');
+                                    } else {
+                                        // User is not human, display an error message
+                                        alert('reCAPTCHA verification failed. Please try again.');
+                                    }
+                                },
+                                error: function(error) {
+                                    console.error('Error:', error);
+                                }
+                            });
+                        }
+                    </script>
 
 
 
