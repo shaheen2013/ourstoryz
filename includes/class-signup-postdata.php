b00c15c49af4860c2a1ae51e3803cff81ceac9ee
imran-bin <engg.imran22@gmail.com>
2024-08-15 18:44:51 +0600

    map script update

--- includes/class-signup-postdata.php
+++ includes/class-signup-postdata.php
@@ -155,68 +155,69 @@ class Signup_Modal_Info
         </div>';
     }
 
-//     public function displayAddLocationSection()
-// {
-//     echo '
-//     <div id="add-location-section" class="w-600 d-none">
-//         <div class="fs-20 divider pb-4">Add a location</div>
-//         <div class="fs-16 mt-20 divider mb-20 pb-20">
-//             Provide the location of your event (approximate is OK).
-//             You’ll need it to test the OurStoryz Geospace features. You can always add this later.
-//         </div>
-//         <img src="./assets/images/add-location.png" alt="event" class="w-100 rounded-4 mb-20">
+    //     public function displayAddLocationSection()
+    // {
+    //     echo '
+    //     <div id="add-location-section" class="w-600 d-none">
+    //         <div class="fs-20 divider pb-4">Add a location</div>
+    //         <div class="fs-16 mt-20 divider mb-20 pb-20">
+    //             Provide the location of your event (approximate is OK).
+    //             You’ll need it to test the OurStoryz Geospace features. You can always add this later.
+    //         </div>
+    //         <img src="./assets/images/add-location.png" alt="event" class="w-100 rounded-4 mb-20">
 
-//         <div class="my-20">
-//             <div class="row">
-//                 <div class="col-6">
-//                     <div id="add-location-change-section" class="h-100 d-flex flex-column justify-content-center">
-//                         <div class="fs-18 text-end mb-2">Your event location</div>
-//                         <div onclick="handleChangeLocation()" id="add-location-change-btn" class="btn-location-change">
-//                             Change
-//                         </div>
-//                     </div>
-//                     <div id="add-location-change-input" class="h-100 flex-column justify-content-center d-none">
-//                         <div class="fs-18 mb-2">Your event location</div>
-//                         <textarea name="location" class="form-control flex-grow-1"></textarea>
-//                     </div>
-//                 </div>
-//                 <div class="col-6">
-//                     <div class="map-area"></div>
-//                 </div>
-//             </div>
-//         </div>
+    //         <div class="my-20">
+    //             <div class="row">
+    //                 <div class="col-6">
+    //                     <div id="add-location-change-section" class="h-100 d-flex flex-column justify-content-center">
+    //                         <div class="fs-18 text-end mb-2">Your event location</div>
+    //                         <div onclick="handleChangeLocation()" id="add-location-change-btn" class="btn-location-change">
+    //                             Change
+    //                         </div>
+    //                     </div>
+    //                     <div id="add-location-change-input" class="h-100 flex-column justify-content-center d-none">
+    //                         <div class="fs-18 mb-2">Your event location</div>
+    //                         <textarea name="location" class="form-control flex-grow-1"></textarea>
+    //                     </div>
+    //                 </div>
+    //                 <div class="col-6">
+    //                     <div class="map-area"></div>
+    //                 </div>
+    //             </div>
+    //         </div>
 
-//         <div class="divider mb-20 pb-20"></div>
+    //         <div class="divider mb-20 pb-20"></div>
 
-//         <div class="d-flex justify-content-between gap-2">
-//             <div onclick="handleSetModal(\'add-dates-section\')" class="btn btn-dark rounded-pill w-100">
-//                 Continue
-//             </div>
+    //         <div class="d-flex justify-content-between gap-2">
+    //             <div onclick="handleSetModal(\'add-dates-section\')" class="btn btn-dark rounded-pill w-100">
+    //                 Continue
+    //             </div>
 
-//             <div onclick="handleSetModal(\'add-dates-section\')" class="btn w-100 btn-dark rounded-pill border-dark border fw-semibold bg-transparent text-dark">
-//                 Don’t know (skip)
-//             </div>
-//         </div>
-//         <div onclick="handleSetModal(\'why-list-location\')" class="text-center text-decoration-underline fs-14 fw-semibold mt-2" type="button">
-//             Why do I need to list a location?
-//         </div>
+    //             <div onclick="handleSetModal(\'add-dates-section\')" class="btn w-100 btn-dark rounded-pill border-dark border fw-semibold bg-transparent text-dark">
+    //                 Don’t know (skip)
+    //             </div>
+    //         </div>
+    //         <div onclick="handleSetModal(\'why-list-location\')" class="text-center text-decoration-underline fs-14 fw-semibold mt-2" type="button">
+    //             Why do I need to list a location?
+    //         </div>
 
-//         <div class="d-flex justify-content-between">
-//             <div class="fs-16 text-end mt-4 text-black" type="button" onclick="handleSetModal(\'give-your-event-name-section\')">
-//                 <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                     <path d="M14.25 9H3.75M3.75 9L9 14.25M3.75 9L9 3.75" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-//                 </svg>
-//                 Back
-//             </div>
-//             <div class="fs-16 text-end mt-4 text-black" type="button" onclick="handleSetModal(\'sorry-to-see-section\')">
-//                 Cancel
-//             </div>
-//         </div>
-//     </div>';
-// }
+    //         <div class="d-flex justify-content-between">
+    //             <div class="fs-16 text-end mt-4 text-black" type="button" onclick="handleSetModal(\'give-your-event-name-section\')">
+    //                 <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    //                     <path d="M14.25 9H3.75M3.75 9L9 14.25M3.75 9L9 3.75" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    //                 </svg>
+    //                 Back
+    //             </div>
+    //             <div class="fs-16 text-end mt-4 text-black" type="button" onclick="handleSetModal(\'sorry-to-see-section\')">
+    //                 Cancel
+    //             </div>
+    //         </div>
+    //     </div>';
+    // }
 
-public function displayAddLocationSection(){
-    echo '
+    public function displayAddLocationSection()
+    {
+        echo '
     <div id="add-location-section" class="w-600 d-none">
         <div class="fs-20 divider pb-4">Add a location</div>
         <div class="fs-16 mt-20 divider mb-20 pb-20">
@@ -225,16 +226,15 @@ public function displayAddLocationSection(){
         </div>
         <img src="./assets/images/add-location.png" alt="event" class="w-100 rounded-4 mb-20">
 
-         <button id="open-map-modal" style="padding: 10px 20px;">Show Map</button>
-  
-    
-      <div id="map-modal" style="display: none; position: fixed; z-index: 1000; left: 0; top: 0; width: 100%; height: 100%; overflow: auto; background-color: rgba(0,0,0,0.4);">
-          <div style="background-color: #fff; margin: 10% auto; padding: 20px; border: 1px solid #888; width: 80%;">
-              <span id="close-map-modal" style="color: #aaa; float: right; font-size: 28px; font-weight: bold;">&times;</span>
-              <input id="location-input" type="text" placeholder="Enter a location" style="width: 100%; padding: 10px; margin-bottom: 10px;">
-              <div id="map" style="height: 400px; width: 100%;"></div>
-          </div>
-      </div>
+        <button id="open-map-modal" style="padding: 10px 20px;">Show Map</button>
+
+        <div id="map-modal" style="display: none; position: fixed; z-index: 1000; left: 0; top: 0; width: 100%; height: 100%; overflow: auto; background-color: rgba(0,0,0,0.4);">
+            <div style="background-color: #fff; margin: 10% auto; padding: 20px; border: 1px solid #888; width: 80%;">
+                <span id="close-map-modal" style="color: #aaa; float: right; font-size: 28px; font-weight: bold;">&times;</span>
+                <input id="location-input" type="text" placeholder="Enter a location" style="width: 100%; padding: 10px; margin-bottom: 10px;">
+                <div id="map" style="height: 400px; width: 100%;"></div>
+            </div>
+        </div>
 
         <div class="divider mb-20 pb-20"></div>
 
@@ -262,9 +262,65 @@ public function displayAddLocationSection(){
                 Cancel
             </div>
         </div>
-    </div>';
+    </div>
 
-}
+    <script>
+        jQuery(document).ready(function($) {
+            // Open the Modal
+            $("#open-map-modal").click(function() {
+                $("#map-modal").show();
+                // Initialize map when the modal is shown
+                initializeAutocomplete();
+            });
+
+            // Close the Modal
+            $("#close-map-modal").click(function() {
+                $("#map-modal").hide();
+            });
+
+            // Close the Modal when clicking outside of the modal content
+            $(window).click(function(event) {
+                if (event.target.id == "map-modal") {
+                    $("#map-modal").hide();
+                }
+            });
+
+            function initializeAutocomplete() {
+                var input = document.getElementById("location-input");
+                var autocomplete = new google.maps.places.Autocomplete(input);
+
+                var map = new google.maps.Map(document.getElementById("map"), {
+                    zoom: 15,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                });
+
+                var marker = new google.maps.Marker({
+                    map: map
+                });
+
+                autocomplete.addListener("place_changed", function() {
+                    var place = autocomplete.getPlace();
+
+                    if (!place.geometry) {
+                        console.log("Returned place contains no geometry");
+                        return;
+                    }
+
+                    // If the place has a geometry, then present it on a map.
+                    if (place.geometry.viewport) {
+                        map.fitBounds(place.geometry.viewport);
+                    } else {
+                        map.setCenter(place.geometry.location);
+                        map.setZoom(17); // Why 17? Because it looks good.
+                    }
+
+                    marker.setPosition(place.geometry.location);
+                    marker.setVisible(true);
+                });
+            }
+        });
+    </script>';
+    }
 
 
     public function displayWhyListLocation()
