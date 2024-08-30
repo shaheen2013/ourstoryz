dcccfbee4917e42c53bf18593f3ba9ac8362f040
imran-bin <engg.imran22@gmail.com>
2024-08-30 16:26:44 +0600

    update

--- includes/class-ourstoryz.php
+++ includes/class-ourstoryz.php
@@ -162,7 +162,7 @@ class ourstoryz
 
 		$this->loader->add_action('init', $plugin_admin, 'custom_ourstoryz_post_type');
 		$this->loader->add_action('init', $plugin_admin, 'create_signup_post_type');
-		$this->loader->add_action( 'init',$plugin_admin, 'custom_register_post_type' );
+ 
     
 		$this->loader->add_action('admin_menu', $plugin_admin, 'custom_ourstoryz_submenu_names');
 		$this->loader->add_action('init', $plugin_admin, 'custom_ourstoryz_taxonomies');
